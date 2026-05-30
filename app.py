883cf26cb614f6bd879ef63c3cc55a42b98dc68a
aastik-codes <aastik.codes@gmail.com>
2026-05-30 13:03:19 +0530

    Move host and port configuration to environment variables

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from config import *
+from config import HOST, PORT
+
 
 app = Flask(__name__)
 
@@ -7,4 +9,4 @@ from routes import *
 
 if __name__ == "__main__":
     startmongo()
-    app.run(debug=True)
+    app.run(host=HOST, port=PORT, debug=True)
--- config.py
+++ config.py
@@ -1,13 +1,15 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+load_dotenv()
 
+HOST = os.getenv("HOST")
+PORT = int(os.getenv("PORT"))
 collection = None
 def startmongo():
     global client
     global db
     global collection
-    load_dotenv()
     mongo_uri = os.getenv("MONGO_URI")
     client = MongoClient(mongo_uri)
     db = client["todos"]
